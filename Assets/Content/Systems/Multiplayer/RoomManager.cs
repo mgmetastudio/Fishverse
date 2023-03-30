817dc87dab6e9531ba740d5569c29778ce1de42f
DK <mr.dener1111@gmail.com>
2023-03-30 15:04:36 +0300

    player spawn

--- Assets/Content/Systems/Multiplayer/RoomManager.cs
+++ Assets/Content/Systems/Multiplayer/RoomManager.cs
@@ -18,17 +18,38 @@ public class RoomManager : MonoBehaviourPunCallbacks
             return;
         }
 
-        int i = 0;
-        foreach (var item in PhotonNetwork.CurrentRoom.Players)
-        {
-            if (item.Value.IsLocal)
-                PhotonNetwork.Instantiate(player_prefab.name, spawn_points[i].position, spawn_points[i].rotation);
-            i++;
-        }
+        // int i = 0;
+        // foreach (var item in PhotonNetwork.CurrentRoom.Players)
+        // {
+        //     if (item.Value.IsLocal)
+        //         PhotonNetwork.Instantiate(player_prefab.name, spawn_points[i].position, spawn_points[i].rotation);
+        //     i++;
+        // }
 
         // PhotonNetwork.Instantiate(player_prefab.name,
         //     spawn_points[Random.Range(0, spawn_points.Length - 1)].position,
         //     spawn_points[Random.Range(0, spawn_points.Length - 1)].rotation);
+
+        var photonView = PhotonView.Get(this);
+        int index = 0;
+        if (PhotonNetwork.IsMasterClient)
+        {
+            foreach (Player pl in PhotonNetwork.PlayerList)
+            {
+                //Debug.Log(pl);
+                photonView.RPC("InstantiationPlayer", pl, index);
+
+                index++;
+            }
+        }
+    }
+
+
+    [PunRPC]
+    void InstantiationPlayer(int index)
+    {
+        PhotonNetwork.Instantiate(player_prefab.name, spawn_points[index].position, spawn_points[index].rotation);
+        // PhotonNetwork.Instantiate(Playerprefabs[value].name, SpawnPoints[index].transform.position, Quaternion.identity, 0);
     }
 
     public void Leave()

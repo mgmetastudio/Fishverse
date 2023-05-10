32ffa2ecfccfb9961029243a910e6fc15dc2f6d7
DK <mr.dener1111@gmail.com>
2023-05-10 14:47:06 +0300

    new inventory wip

--- Assets/_Scripts/OpenWorld/OpenWorldShop.cs
+++ Assets/_Scripts/OpenWorld/OpenWorldShop.cs
@@ -14,14 +14,19 @@ public class OpenWorldShop : MonoBehaviour
 
     void OnPlayerEnter(GameObject playerObj)
     {
-        var inv = playerObj.GetComponent<Inventory>();
+        var inv = playerObj.GetComponent<Opsive.UltimateInventorySystem.Core.InventoryCollections.Inventory>();
+        var shopInv = playerObj.GetComponentInChildren<Opsive.UltimateInventorySystem.UI.Menus.ShopMenuOpener>();
+        // print("INV: " + inv);
+        // print("Shop: " + shop);
+        
+        shopInv.Open(inv);
 
-        int amount = 0;
-        foreach (var item in inv.fishInv)
-        {
-            string resultString = System.Text.RegularExpressions.Regex.Match(item.FishRetailValue.text, @"\d+").Value;
-            amount += System.Int32.Parse(resultString);
-        }
-        inv.SellAllFish(amount);
+        // int amount = 0;
+        // foreach (var item in inv.fishInv)
+        // {
+        //     string resultString = System.Text.RegularExpressions.Regex.Match(item.FishRetailValue.text, @"\d+").Value;
+        //     amount += System.Int32.Parse(resultString);
+        // }
+        // inv.SellAllFish(amount);
     }
 }
